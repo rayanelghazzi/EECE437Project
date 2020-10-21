247ea15b862f7230e636f7883388d24a6c8e70b3
Emmanuel Nassour <emmanuel.nassour123@gmail.com>
2020-10-21 21:11:30 +0300

    add functions to TransactionService and UserClient

--- EECE437Project/EECE437Project/Services/ITransactionService.cs
+++ EECE437Project/EECE437Project/Services/ITransactionService.cs
@@ -3,5 +3,11 @@ namespace EECE437Project.Services
 {
     public interface ITransactionService
     {
+        void CreateDemand();
+        void EditDemand();
+        void DeleteDemand();
+        void CreateTask();
+        void EditTask();
+        void DeleteTask();
     }
 }
--- EECE437Project/EECE437Project/Services/TransactionService.cs
+++ EECE437Project/EECE437Project/Services/TransactionService.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using EECE437Project.Services;
 
 namespace EECE437Project.Components
 {
-    class TransactionService
+    public class TransactionService : ITransactionService
     {
+        public void CreateDemand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDemand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditDemand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditTask()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
