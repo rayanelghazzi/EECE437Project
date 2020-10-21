bc030494d631205d09d3949d4d136251ec112c6e
Rayan El Ghazzi <rayanelghazzi@hotmail.com>
2020-10-21 21:39:41 +0300

    minor changes

---
+++ EECE437Project/EECE437Project/Clients/IClient.cs
@@ -0,0 +1,17 @@
+﻿using EECE437Project.DataContract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EECE437Project.Clients
+{
+    public interface IUserClient
+    {
+        List<Demand> GetDemands();
+        Demand GetDemand(string demandId);
+        void DeleteDemand(string demandId);
+        List<Task> GetTasks();
+        Task GetTask(string demandId);
+        void DeleteTask(string demandId);
+    }
+}
