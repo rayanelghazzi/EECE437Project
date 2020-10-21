d095a5489361403b4d6a9f931a27e818d80a4f31
Rayan El Ghazzi <rayanelghazzi@hotmail.com>
2020-10-21 22:27:53 +0300

    Added function definitions to ITransactionService

--- EECE437Project/EECE437Project/Services/ITransactionService.cs
+++ EECE437Project/EECE437Project/Services/ITransactionService.cs
@@ -3,10 +3,18 @@ namespace EECE437Project.Services
 {
     public interface ITransactionService
     {
+        void GetDemand();
+        void GetDemands();
         void CreateDemand();
         void EditDemand();
-        void DeleteDemand();
+        void CancelDemand();
+        void GetContribution();
+        void GetContributions();
         void EditContribution();
         void CancelContribution();
+        void AnswerDemand();
+        void GetProcess();
+        void GetProcesses();
+        void ValidateDelivery(); //Validate the delivery code + Complete Contribution
     }
 }
